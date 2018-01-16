6a9f9a6acaa3be879d1c0b01519c3d2951cc37de
lanleilin <2476081528@qq.com>
2018-01-16 18:04:14 +0800

    ts-vue first commit
    Change-Id: Id80fee50bb335ddb818e3f666d997599abd7250f

---
+++ src/components/Hello.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    {{welcomeMsg}}
+    <Test></Test>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Test from './Test.vue'
+export interface Data {
+  welcomeMsg: string,
+  appName: string
+}
+
+export default Vue.extend({
+  data (): Data {
+    return {
+      welcomeMsg: '这是TS组件',
+      appName: 'ts+vue'
+    }
+  },
+
+  components: {
+    Test
+  }
+})
+</script>
+
+<style>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    {{value}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      value: '这是ES6组件'
+    }
+  }
+}
+</script>
+
+<style>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
